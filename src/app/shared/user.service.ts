786768e4a39889e6447230c0338a5f9ff96ba581
vinodprabhakar <vin62vic@gmail.com>
2019-05-01 16:22:24 +0530

    Update user.service.ts

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class UserService {
-private user = new BehaviorSubject<string>('Sathish');
+private user = new BehaviorSubject<string>('Trainee');
 cast = this.user.asObservable();
 
   constructor() { }
